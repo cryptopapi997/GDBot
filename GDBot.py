d562f9b59979aabfe1d49bcff292c1980f824e3e
Nicolas Schapeler <nicoschap@live.fr>
2019-08-12 18:14:11 +0200

    added a couple comments

--- GDBot.py
+++ GDBot.py
@@ -1,13 +1,14 @@
 import numpy as np
-import pyscreenshot as ImageGrab
+import pyscreenshot
 import cv2
 
 
 def screen_record(): 
     while(True):
-        # 800x600 windowed mode
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        # 800x600 (size of GD)
+        printscreen =  np.array(pyscreenshot.grab(bbox=(0,40,800,640)))
         cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        #press q to exit screen recording
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
