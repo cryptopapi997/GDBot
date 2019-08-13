ebec2a9ed480d03adeec19fcf7a25bed395818a1
Nicolas Schapeler <nicoschap@live.fr>
2019-08-13 17:48:13 +0200

    speed boost

--- GDBot.py
+++ GDBot.py
@@ -7,7 +7,10 @@ def screen_record():
     while(True):
         # 550x600 (size of GD without the things behind the cube being recorded)
         printscreen =  np.array(pyscreenshot.grab(bbox=(250,40,800,640)))
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        # simplify image
+        gray_printscreen = cv2.cvtColor(printscreen, cv2.COLOR_BGR2GRAY)
+        gray_printscreen = cv2.Canny(gray_printscreen, threshold1 = 200, threshold2=300)
+        cv2.imshow('window',gray_printscreen)
         #press q to exit screen recording
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
