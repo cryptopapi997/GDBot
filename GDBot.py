b7c927168cb61670850e6ad2190b631222a9dfc5
Nicolas Schapeler <nicoschap@live.fr>
2019-08-13 17:40:37 +0200

    made the screen a bit smaller

--- GDBot.py
+++ GDBot.py
@@ -5,8 +5,8 @@ import cv2
 
 def screen_record(): 
     while(True):
-        # 800x600 (size of GD)
-        printscreen =  np.array(pyscreenshot.grab(bbox=(0,40,800,640)))
+        # 550x600 (size of GD without the things behind the cube being recorded)
+        printscreen =  np.array(pyscreenshot.grab(bbox=(250,40,800,640)))
         cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         #press q to exit screen recording
         if cv2.waitKey(25) & 0xFF == ord('q'):
